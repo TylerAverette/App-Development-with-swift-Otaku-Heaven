7a0a333f61853eca76e3a8782efa27f2ed291a70
TylerAverette <44883616+TylerAverette@users.noreply.github.com>
2019-10-30 14:28:39 -0500

    Add files via upload

---
+++ RainbowTabs/RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by Student User on 4/25/19.
+//  Copyright © 2019 Student User. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+}
---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Student User on 4/25/19.
+//  Copyright © 2019 Student User. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = "1"
+        tabBarItem.badgeValue = nil
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
