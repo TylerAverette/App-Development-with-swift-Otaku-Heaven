3e5e93ab1d3b43190804b2b631d049dbda353e59
TylerAverette <44883616+TylerAverette@users.noreply.github.com>
2019-10-30 14:52:58 -0500

    Add files via upload

---
+++ Meal Tracker 2/Meal Tracker/Model/Food.swift
@@ -0,0 +1,14 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Student User on 8/28/19.
+//  Copyright © 2019 Student User. All rights reserved.
+//
+
+import Foundation
+
+struct Food{
+    var name: String
+    var description: String
+}
---
+++ Meal Tracker 2/Meal Tracker/food.swift
@@ -0,0 +1,9 @@
+//
+//  food.swift
+//  Meal Tracker
+//
+//  Created by Student User on 8/28/19.
+//  Copyright © 2019 Student User. All rights reserved.
+//
+
+import Foundation
